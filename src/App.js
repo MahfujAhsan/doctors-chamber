8053d427939e6d032ca8c9e8b9df0be545cf5bc1
MahfujAhsan <ahsanmahfuj@gmail.com>
2022-05-14 21:49:08 +0600

    user appointment data collected

--- src/App.js
+++ src/App.js
@@ -4,19 +4,27 @@ import About from './Pages/About/About';
 import Appointment from './Pages/Appointment/Appointment';
 import Home from './Pages/Home/Home';
 import Login from './Pages/Login/Login';
+import RequireAuth from './Pages/Login/RequireAuth';
+import SignUp from './Pages/Login/SignUp';
 import Navbar from './Pages/Shared/Navbar';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <div className='max-w-7xl mx-auto'>
-      <Navbar/>
+      <Navbar />
       <Routes>
-        <Route path='/' element={<Home/>}></Route>
-        <Route path='/appointment' element={<Appointment/>}></Route>
-        <Route path='/about' element={<About/>}></Route>
-
-        <Route path='/login' element={<Login/>}></Route>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/appointment' element={
+          <RequireAuth>
+            <Appointment />
+          </RequireAuth>}></Route>
+        <Route path='/about' element={<About />}></Route>
+        <Route path='/login' element={<Login />}></Route>
+        <Route path='/signup' element={<SignUp />}></Route>
       </Routes>
+      <ToastContainer />
     </div>
   );
 }
--- src/Pages/Appointment/Service.js
+++ src/Pages/Appointment/Service.js
@@ -1,19 +1,19 @@
 import React from 'react';
 
 const Service = ({ service, setTreatment }) => {
-    const {name, slots} = service;
+    const { name, slots } = service;
     return (
-        <div class="card lg:max-w-lg bg-base-100 shadow-xl">
-            <div class="card-body">
-                <h2 class="card-title">{name}</h2>
+        <div className="card lg:max-w-lg bg-base-100 shadow-xl">
+            <div className="card-body text-center">
+                <h2 className="text-xl font-bold">{name}</h2>
                 <p>
                     {
                         slots.length > 0 ? <span>{slots[0]}</span> : <span className='text-red-600'>Try Another Date</span>
                     }
                 </p>
                 <p>{slots.length} {slots.length > 1 ? 'spaces' : 'space'} available</p>
-                <div class="card-actions justify-center">           
-                    <label disabled={slots.length === 0} onClick={() => setTreatment(service)} for="booking-modal" class="btn btn-secondary text-white text-uppercase font-bold">Book Appointment</label>
+                <div className="card-actions justify-center">
+                    <label htmlFor="booking-modal" disabled={slots.length === 0} onClick={() => setTreatment(service)} className="btn btn-sm bg-gradient-to-r from-secondary to-primary btn-secondary text-white text-uppercase font-bold">Book Appointment</label>
                 </div>
             </div>
         </div>
